5e81eb21bef7e96c603315b56c8a56374d5d8966
Bradley Showen <bshowen@me.com>
2020-05-02 07:13:31 -0400

    creates static_pages_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'searches/new'
+  get 'searches/show'
+  get 'searches/create'
+  get 'static_pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
