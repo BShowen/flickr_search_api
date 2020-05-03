f0fe6fda15c685db79fbe2fdce44c8a0b50b92f0
Bradley Showen <bshowen@me.com>
2020-05-02 20:58:45 -0400

    cleanes up searches_controller/views

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -2,17 +2,11 @@ class SearchesController < ApplicationController
   def new
   end
 
-  def show
-  end
-
   def create
     @flickr_user = FlickrApi.new(username: params[:search][:username])
     if !@flickr_user.valid?
       flash.now[:alert] = @flickr_user.errors.first 
-      render :new
-    else
-      render :show
     end
+    render :new
   end
-
 end
