a167bcf0f04e260021661d09aeeccea3fa7ea3dc
Bradley Showen <bshowen@me.com>
2020-05-10 11:41:07 -0400

    adds basic testing

--- Gemfile
+++ Gemfile
@@ -39,6 +39,9 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+
+  # use minitest-reporters for styling test output
+  gem 'minitest-reporters', '~> 1.4.2'
 end
 
 group :development do
--- test/controllers/searches_controller_test.rb
+++ test/controllers/searches_controller_test.rb
@@ -2,18 +2,40 @@ require 'test_helper'
 
 class SearchesControllerTest < ActionDispatch::IntegrationTest
   test "should get new" do
-    get searches_new_url
+    get root_url
     assert_response :success
   end
 
-  test "should get show" do
-    get searches_show_url
-    assert_response :success
+  test "flash response" do 
+    get root_url
+
+    # flash should render with blank search
+    post root_url, params: { search: { username: ' '}}
+    assert_not flash.empty?
+
+    # flash should render with a search for a user who has photos
+    post root_url, params: { search: { username: 'bradleh2' }}
+    assert_not flash.empty?
+
+    # flash should render with a search for a user who doesnt have photos
+    post root_url, params: { search: { username: 'sir'}}
+    assert_not flash.empty?
+
+    # flash should render with a search for a user who doesnt exits
+    post root_url, params: { search: { username: 'foo-woo-bar-baz' }}
+    assert_not flash.empty?
   end
 
-  test "should get create" do
-    get searches_create_url
-    assert_response :success
+  test "photo rendering and links" do
+    post root_url, params: { search: { username: 'bradleh2' }}
+    assert_not flash.empty?
+    # assert_select 'div.d-flex>div.py-2', minimum: 1 # this is the same as doing the next three lines of code. 
+    assert_select 'div.d-flex' do
+      assert_select 'div.py-2', minimum: 1 do 
+        # test for presence of a link
+        assert_select 'a', minimum: 1
+      end
+    end
   end
 
 end
